26ca188254822c8c2041f96754455b40e91cb8bb
David Y. Stephenson <david@davidystephenson.com>
2025-06-14 11:05:54 -0400

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    async function download () {
+      const response = await fetch('http://localhost:4000/count')
+      const data = await response.json()
+      console.log('data', data)
+      setCount(data.count)
+    }
+    download()
+  }, [])
+
+  async function handleClick () {
+    const response = await fetch('http://localhost:4000/count', {
+      method: 'PATCH'
+    })
+    const data = await response.json()
+    console.log('increment', data)
+    setCount(data.count)
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        count is {count}
+      </button>
+    </>
+  )
+}
+
+export default App
